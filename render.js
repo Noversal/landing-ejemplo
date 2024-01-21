29c6e5f1b479952a62cfa57cb34e8f76cc58f77b
Noversal <dionofrio_franco@hotmail.com>
2024-01-21 18:51:07 -0300

    fix: rename file import

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 import Header from './layouts/header'
-import Footer from './layouts/Footer'
+import Footer from './layouts/footer'
 import Main from './layouts/Main'
 
 export function render (root) {
